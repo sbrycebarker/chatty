34ee6e5946a61b49fdcb94f386a63a70bd37bbb1
sbrycebarker <sbrycesti@gmail.com>
2017-03-21 15:52:04 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+var express = require('express')
+var bodyParser = require('body-parser')
 
+var app = express();
+
+  app.use(express.static('assets'))
+  app.use(bodyParser.json())
+
+
+
+  var messages = []
+
+  app.post('/messages', function(req, res, next) {
+    messages.push( { message: req.body.message, time: new Date() });
+    res.status(200).json({messages: messages})
+  });
+
+  app.get('/messages', function(req, res, next) {
+    res.status(200).json({messages: messages});
+  });
+
+app.listen(3000, function(){
+  console.log("welcome to port 3000")
+});
